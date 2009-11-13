07eda2ea96c8419ff551d843cd2d48b7fa071ac8
Paul Miller <paul.e.miller@wmich.edu>
2009-11-13 16:28:59 -0500

    pfft, part way though various XBeePacket upgrades

---
+++ Address64.java
@@ -0,0 +1,5 @@
+
+public class Address64 {
+    Address64(byte SH[], byte SL[]) {
+    }
+}
--- XBeePacket.java
+++ XBeePacket.java
@@ -8,17 +8,17 @@ public class XBeePacket {
         return new String(packet);
     }
 
-    public void set_tx(char seqno, String _payload) throws PayloadException {
+    public void set_tx(byte seqno, Address64 a, String _payload) throws PayloadException {
         byte payload[] = _payload.getBytes();
 
         if( payload.length > 100 )
             throw new PayloadException("asked to packetize " + payload.length + " bytes, but payloads are restricted to 100 bytes");
 
-        packet = new byte[ payload.length + 8 ];
+        packet = new byte[ payload.length + 9 ];
 
         // the payload bytes:
         for(int i=0; i<payload.length; i++) {
-            packet[i+7] = payload[i];
+            packet[i+8] = payload[i];
 
             /* if( payload[i] == 0x7e )
             /*     throw new PayloadException("invalid character at byte-" + i + " of payload 0x7e characters may not appear in a packet payload");
@@ -29,6 +29,11 @@ public class XBeePacket {
         packet[0] = 0x7e;
         packet[1] = (byte) ((0xff00 & packet.length) >> 8);
         packet[2] = (byte) (0xff & packet.length);
+        packet[3] = 0x00; // Tx packet
+        packet[4] = seqno;
+        packet[5] = a.MSB();
+        packet[6] = a.LSB();
+        packet[7] = 0x0; // 0x01 is disable ACK and 0x04 is use broadcast, neither interest us
     }
 
     public static XBeePacket tx(char seqno, String payload) throws PayloadException {
