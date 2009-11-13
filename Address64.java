e371a173f995ce5be4e55809b497a64291e26b51
Paul Miller <paul.e.miller@wmich.edu>
2009-11-13 18:24:58 -0500

    little things, trying to get these to work

--- Address64.java
+++ Address64.java
@@ -1,5 +1,15 @@
 
 public class Address64 {
-    Address64(byte SH[], byte SL[]) {
+    byte addr[];
+
+    Address64(byte SH[], byte SL[]) throws Address64Exception {
+        if( SH.length != 4 ) throw new Address64Exception("SH address must be 4 bytes (not " + SH.length + ")");
+        if( SL.length != 4 ) throw new Address64Exception("SL address must be 4 bytes (not " + SL.length + ")");
+
+        addr = new byte[8];
+        for(int i=0; i<4; i++) {
+            addr[i]   = SH[i];
+            addr[i+4] = SL[i];
+        }
     }
 }
---
+++ Address64Exception.java
@@ -0,0 +1,4 @@
+
+public class Address64Exception extends IllegalArgumentException {
+    public Address64Exception(String error) { super(error); } 
+}
