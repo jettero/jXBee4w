aecb0ef62399d6a6623237d4af74d55aeea19124
Paul Miller <paul.e.miller@wmich.edu>
2009-11-13 10:05:11 -0500

    using this formula to dump packets to a file

--- test.java
+++ test.java
@@ -1,9 +1,21 @@
-//import XBeeAPIPacket;
+import java.io.*;
 
 public class test {
     public static void main(String[] args) {
         XBeePacketizer p = new XBeePacketizer();
-        XBeePacket q[] = p.build_tx("supz");
+        XBeePacket q[]   = p.build_tx("supz");
 
+        try {
+        // Create file 
+            FileWriter fstream = new FileWriter("out.txt");
+            BufferedWriter out = new BufferedWriter(fstream);
+
+            out.write("Hello Java");
+            out.close();
+        }
+
+        catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+        }
     }
 }
