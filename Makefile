9b1fb1918b01f05348018139a906e4cc39c08d2a
Paul Miller <paul.e.miller@wmich.edu>
2009-11-15 16:56:22 -0500

    new way to build test so the .last is preserved even on error

--- Makefile
+++ Makefile
@@ -5,8 +5,11 @@ default_test=packetizer_test
 run_last_test:
 	@-if [ -f .last ]; then make `cat .last`; else make $(default_test); fi
 
-%_test: %_test.class
-	@echo $@ > .last; echo " >> RUNNING $@ << "; java $@
+%_test:
+	@echo $@ > .last
+	@make --no-print-directory $@.class
+	@echo " >> RUNNING $@ << "
+	@java $@
 
 clean:
 	git clean -dfx
---
+++ checksum_test.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class address_test {
+    public static void main(String[] args) {
+        Address64 a = new Address64("00:11:22:33:44:55:66:77");
+        Address64 b = new Address64("88:99:aa:bb:cc:dd:ee:ff");
+        Address64 c = new Address64("11:05:19:73:11:05:19:73");
+
+        System.out.println( a.toText() );
+        System.out.println( b.toText() );
+        System.out.println( c.toText() );
+
+        try {
+            FileOutputStream out = new FileOutputStream("dump.txt");
+
+            out.write( a.getBytes() );
+            out.write( b.getBytes() );
+            out.write( c.getBytes() );
+
+            out.close();
+        }
+
+        catch (Exception e) {
+            System.err.println("file output stream exception: " + e.getMessage());
+        }
+
+        try {
+            String cmd[]   = {"xxd", "dump.txt"};
+            Process prcs   = Runtime.getRuntime().exec(cmd);
+            InputStream in = prcs.getInputStream();
+
+            int ch;
+            StringBuffer sb = new StringBuffer(512);
+            while((ch=in.read())!=-1)
+                sb.append((char)ch);
+
+            System.out.print(sb.toString());
+        }
+
+        catch (Exception e) {
+            System.err.println("exec() exception: " + e.getMessage());
+        }
+    }
+}
