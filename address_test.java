d7bb5b2f579535c990623851d25e8965a749b563
Paul Miller <paul.e.miller@wmich.edu>
2009-11-15 08:56:18 -0500

    make java exec xxd

--- address_test.java
+++ address_test.java
@@ -22,7 +22,24 @@ public class address_test {
         }
 
         catch (Exception e) {
-            System.err.println("Error: " + e.getMessage());
+            System.err.println("file output stream exception: " + e.getMessage());
+        }
+
+        try {
+            String cmd[]   = {"xxd", "dump.txt"};
+            Process prcs   = Runtime.getRuntime().exec(cmd);
+            InputStream in = prcs.getInputStream();
+
+            int ch;
+            StringBuffer sb = new StringBuffer(512);
+            while((ch=in.read())!=-1)
+                sb.append((char)ch);
+
+            System.out.print(sb.toString());
+        }
+
+        catch (Exception e) {
+            System.err.println("exec() exception: " + e.getMessage());
         }
     }
 }
