7193c40b28034090e3fd6643227bde830f960dca
Paul Miller <paul.e.miller@wmich.edu>
2009-11-15 14:07:10 -0500

    this seems all set... now how about the checksum bit

--- XBeePacket.java
+++ XBeePacket.java
@@ -17,17 +17,12 @@ public class XBeePacket {
 
     public void set_tx(byte seqno, Address64 dst, String _payload) throws PayloadException {
         byte payload[] = _payload.getBytes();
+        byte _dstb[]   = dst.getBytes();
 
         if( payload.length > 100 )
             throw new PayloadException("asked to packetize " + payload.length + " bytes, but payloads are restricted to 100 bytes");
 
         packet = new byte[ payload.length + 9 ];
-
-        // the payload bytes:
-        for(int i=0; i<payload.length; i++)
-            packet[i+8] = payload[i];
-
-        byte _dstb[] = dst.serialize();
 
         // frame header:
         packet[0]  = 0x7e;
@@ -35,18 +30,15 @@ public class XBeePacket {
         packet[2]  = (byte) (0xff & packet.length);
         packet[3]  = 0x00; // Tx packet
         packet[4]  = seqno;
+        packet[13] = 0x0; // 0x01 is disable ACK and 0x04 is use broadcast, neither interest us
 
         // destination address
-        packet[5]  = _dstb[0];
-        packet[6]  = _dstb[1];
-        packet[7]  = _dstb[2];
-        packet[8]  = _dstb[3];
-        packet[9]  = _dstb[4];
-        packet[10] = _dstb[5];
-        packet[11] = _dstb[6];
-        packet[12] = _dstb[7];
+        for(int i=0; i<8; i++)
+            packet[5+i] = _dstb[0+i]; // 5-12
 
-        packet[13] = 0x0; // 0x01 is disable ACK and 0x04 is use broadcast, neither interest us
+        // the payload bytes:
+        for(int i=0; i<payload.length; i++)
+            packet[i+14] = payload[i]; // 14-n
     }
 }
 
