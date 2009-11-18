6c2dadf85526d8776be6dc754922a3710cab2289
Paul Miller <paul.e.miller@wmich.edu>
2009-11-17 19:03:22 -0500

    this symbol not found error makes no sense

--- XBeeConfig.java
+++ XBeeConfig.java
@@ -1,40 +1,52 @@
 import gnu.io.*;
 import java.io.*;
+import java.util.*;
 
 public class XBeeConfig {
     InputStream  in;
     OutputStream out;
+    String s[];
 
-    private void connect(String portName, int speed) {
-        try {
-            CommPortIdentifier portIdentifier = CommPortIdentifier.getPortIdentifier(portName);
+    XBeeConfig(String portName, int speed) throws NoSuchPortException, PortInUseException, UnsupportedCommOperationException, IOException {
+        CommPortIdentifier portIdentifier = CommPortIdentifier.getPortIdentifier(portName);
+        CommPort commPort                 = portIdentifier.open(this.getClass().getName(), 2000);
 
-        } catch(NoSuchPortException e) {
-            System.out.println("Error: " + e.getMessage());
-        }
+        if ( commPort instanceof SerialPort ) {
+            SerialPort serialPort = (SerialPort) commPort;
+            serialPort.setSerialPortParams(speed, SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
 
-
-        if ( portIdentifier.isCurrentlyOwned() ) {
-            System.out.println("Error: Port is currently in use");
+            in  = serialPort.getInputStream();
+            out = serialPort.getOutputStream();
 
         } else {
-            CommPort commPort = portIdentifier.open(this.getClass().getName(), 2000);
-            
-            if ( commPort instanceof SerialPort ) {
-                SerialPort serialPort = (SerialPort) commPort;
-                serialPort.setSerialPortParams(speed, SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
-                
-                in  = serialPort.getInputStream();
-                out = serialPort.getOutputStream();
-
-            } else {
-                System.out.println("Error: Only serial ports are handled by this example.");
-            }
-        }     
+            throw new UnsupportedCommOperationException("\"" + portName + "\" is probably not a serial port");
+        }
     }
 
-    public void config(String portName, int speed, String configs[]) {
-        Thread.sleep(1);
-        this.out.write("+++".getBytes());
+    public byte[] send_and_recv(String toSend) throws IOException {
+        this.out.write(toSend.getBytes());
+
+        int retries = 10;
+        while( this.in.available() < 1 && retries-->0 )
+            try { Thread.sleep(250); }
+            catch(InterruptedException e) { /* don't really care if it doesn't work... maybe a warning should go here */ }
+
+        List<Integer>l = new ArrayList<Integer>();
+        Integer aByte;
+        while( (aByte = this.in.read()) > -1 )
+            l.add( aByte );
+
+        byte b[] = new byte[ l.size() ];
+        for(int i=0; i<l.size(); i++)
+            b[i] = l.get(i).byteValue();
+
+        return b;
+    }
+
+    public void config(String configs[]) throws IOException {
+        try { Thread.sleep(1000); } catch (InterruptedException e) {} // just ignore it if it gets interrupted
+        byte b[] = this.send_and_recv("+++");
+
+        System.out.println( new String(b) );
     }
 }
--- config_test.java
+++ config_test.java
@@ -2,9 +2,7 @@
 public class config_test {
     public static void main(String[] args) {
         try {
-            XBeeConfig c = (new XBeeConfig()).connect(args[0], 115200);
-
-            c.config("", "", "");
+            XBeeConfig c; // = new XBeeConfig(args[0], 115200);
         }
 
         catch ( Exception e ) {
