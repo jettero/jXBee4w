91f0d9c77cb48435c37ec246b6d4ee7d72d5cbd6
Paul Miller <paul.e.miller@wmich.edu>
2009-11-18 06:59:09 -0500

    try to build a XBeeConfig object

--- config_test.java
+++ config_test.java
@@ -2,7 +2,7 @@
 public class config_test {
     public static void main(String[] args) {
         try {
-            XBeeConfig c; // = new XBeeConfig(args[0], 115200);
+            XBeeConfig c = new XBeeConfig(args[0], 115200);
         }
 
         catch ( Exception e ) {
