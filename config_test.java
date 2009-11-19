452c8a8dc4908a8fefe9ed5fccc5c0115981b2ad
Paul Miller <paul.e.miller@wmich.edu>
2009-11-18 19:25:29 -0500

    redoing the error handling

--- config_test.java
+++ config_test.java
@@ -9,19 +9,37 @@ public class config_test {
         try {
             XBeeConfig c = new XBeeConfig(port, 115200);
 
+            /*
             c.debug = true;
 
-            String conf[] = { "ATRE", "ATBD7", "ATAP1" };
-            String res[]  = c.config(conf);
+            try {
+                String conf[] = { "ATRE" }; //{, "ATBD7", "ATAP1" };
+                String res[]  = c.config(conf);
 
-            for(int i=0; i<conf.length; i++)
-                System.out.println(conf[i] + " result: " + res[i]);
+                for(int i=0; i<conf.length; i++)
+                    System.out.println(conf[i] + " result: " + res[i]);
+
+            } catch( XBeeConfigException e ) {
+            }
+            */
 
             c.close();
         }
 
-        catch ( Exception e ) {
-            System.err.println("ERROR configuring modem: " + e.getMessage());
+        catch(gnu.io.NoSuchPortException e) {
+            System.err.println("ERROR opening port: No Such Port Error");
+        }
+
+        catch(gnu.io.PortInUseException e) {
+            System.err.println("ERROR opening port: port in use");
+        }
+
+        catch(gnu.io.UnsupportedCommOperationException e) {
+            System.err.println("ERROR opening port: unsupported operation ... " + e.getMessage());
+        }
+
+        catch(java.io.IOException e) {
+            System.err.println("ERROR opening port: unsupported operation ... " + e.getMessage());
         }
     }
 }
