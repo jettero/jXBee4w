390b1aa75471abe58ba8e0cab0c120f6ef2069a6
Paul Miller <paul.e.miller@wmich.edu>
2009-11-20 08:58:50 -0500

    current skel compiles... neds more work though

--- XBeeHandle.java
+++ XBeeHandle.java
@@ -13,7 +13,7 @@ public class XBeeHandle {
     protected void finalize() throws Throwable { this.close(); }
     public    void close() { commPort.close(); }
 
-    XBeeHandle(CommPortIdentifier portIdentifier, int speed, boolean _debug, PacketEvent callback) throws NoSuchPortException, PortInUseException, UnsupportedCommOperationException, IOException {
+    XBeeHandle(CommPortIdentifier portIdentifier, int speed, boolean _debug, PacketRecvEvent callback) throws NoSuchPortException, PortInUseException, UnsupportedCommOperationException, IOException {
         debug = _debug;
         commPort = portIdentifier.open(this.getClass().getName(), 2000);
 
@@ -24,24 +24,24 @@ public class XBeeHandle {
             in  = serialPort.getInputStream();
             out = serialPort.getOutputStream();
 
-            (new Thread(new PacketReader(in, callback)).start();
+            (new Thread(new PacketReader(in, callback))).start();
 
         } else {
             throw new UnsupportedCommOperationException("\"" + portIdentifier.getName() + "\" is probably not a serial port");
         }
     }
 
-    public static XBeeHandle newFromPortName(String portName, int speed, boolean debug) throws NoSuchPortException, PortInUseException, UnsupportedCommOperationException, IOException {
+    public static XBeeHandle newFromPortName(String portName, int speed, boolean debug, PacketRecvEvent callback) throws NoSuchPortException, PortInUseException, UnsupportedCommOperationException, IOException {
         CommPortIdentifier portIdentifier = CommPortIdentifier.getPortIdentifier(portName);
 
-        return new XBeeHandle(portIdentifier, speed, debug);
+        return new XBeeHandle(portIdentifier, speed, debug, callback);
     }
 
     private static class PacketReader implements Runnable {
-        private PacketEvent ev;
+        private PacketRecvEvent ev;
         private InputStream in;
         
-        public PacketReader (InputStream in, PacketEvent callback) {
+        public PacketReader (InputStream in, PacketRecvEvent callback) {
             this.in = in;
             this.ev = callback;
         }
@@ -49,27 +49,45 @@ public class XBeeHandle {
         public void run() {
             ByteBuffer b = ByteBuffer.wrap(new byte[1024]);
             boolean inPkt = false;
+            int aByte;
 
             while(true) {
-                while( (aByte = this.in.read()) > -1 ) {
-                    if( inPkt ) {
-                        try { b.put( (byte) aByte ); }
-                        catch(BufferOverflowException e) { /* huh... just ignore this see if we get any more */ }
+                try {
+                    while( (aByte = this.in.read()) > -1 ) {
 
-                        // TODO: do something once we have enough for a packet
-                        // probably build a packet and notify
+                        if( inPkt ) {
+                            try { b.put( (byte) aByte ); }
+                            catch(BufferOverflowException e) { /* huh... just ignore this for now */ }
 
-                        if( 
+                            // TODO: do something once we have enough for a packet
+                            // probably build a packet and notify
 
-                    } else {
+                            // XBeePacket p = new XBeePacket();
+                            // ev.recvPacket(p);
+                            // inPkt = false;
+
+                        } else {
+                            if( aByte == 0x7e ) {
+                                // hey, a new frame (hopefully)
+                                b.clear();
+                                try { b.put( (byte) aByte ); }
+                                catch(BufferOverflowException e) { /* ignore */ }
+                                inPkt = true;
+                            }
+                        }
+
                     }
+                } 
+
+                catch(IOException e) {
+                    inPkt = false;
+                    // pfft.  Just start over.
                 }
             }
         }
     }
 
-
-    public byte[] send_packet(XBeePacket p) throws IOException {
+    public void send_packet(XBeePacket p) throws IOException {
         /// send a packet... this is pretty straight forward
     }
 }
