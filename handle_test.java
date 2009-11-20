f6844e0c3af72fbe33a09fe572f7f79bd81f3a1e
Paul Miller <paul.e.miller@wmich.edu>
2009-11-20 10:35:02 -0500

    course, you can't actually use this from main()

--- handle_test.java
+++ handle_test.java
@@ -3,16 +3,21 @@ import java.util.*;
 import java.util.regex.*;
 
 public class handle_test implements PacketRecvEvent {
-    public static void recvPacket() {
+    public void recvPacket(XBeePacket p) {
         System.out.println("wow, recved!!");
     }
 
-    public static void main(String[] args) {
-        XBeeHandle h  = XBeeHandle();
+    public void go() {
+        XBeeHandle h  = XBeeHandle.newFromPortName("COM8", 115200, true, this);
         XBeePacket sl = XBeePacket.at("SL");
         XBeePacket sh = XBeePacket.at("SH");
 
         h.send_packet(sl);
         h.send_packet(sh);
+    }
+
+    public static void main(String[] args) {
+        handle_test h = new handle_test();
+        h.go();
     }
 }
