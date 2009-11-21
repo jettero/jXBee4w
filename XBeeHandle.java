e3e11640496461ddcd4c7e8a2495235a60f42695
Paul Miller <paul.e.miller@wmich.edu>
2009-11-21 08:06:31 -0500

    change the logic of this a little so it can be closed

--- XBeeHandle.java
+++ XBeeHandle.java
@@ -9,9 +9,14 @@ public class XBeeHandle {
     private InputStream  in;
     private OutputStream out;
     private CommPort commPort;
+    private PacketReader packetReader;
+    private Thread _prThread;
 
     protected void finalize() throws Throwable { this.close(); }
-    public    void close() { commPort.close(); }
+    public    void close() {
+        packetReader.close();
+        commPort.close();
+    }
 
     XBeeHandle(CommPortIdentifier portIdentifier, int speed, boolean _debug, PacketRecvEvent callback) throws NoSuchPortException, PortInUseException, UnsupportedCommOperationException, IOException {
         debug = _debug;
@@ -24,7 +29,9 @@ public class XBeeHandle {
             in  = serialPort.getInputStream();
             out = serialPort.getOutputStream();
 
-            (new Thread(new PacketReader(in, callback))).start();
+            packetReader = new PacketReader(in, callback);
+            _prThread    = new Thread(packetReader);
+            _prThread.start();
 
         } else {
             throw new UnsupportedCommOperationException("\"" + portIdentifier.getName() + "\" is probably not a serial port");
@@ -45,53 +52,69 @@ public class XBeeHandle {
             this.in = in;
             this.ev = callback;
         }
+
+        public void close() {
+            System.out.println("[debug] packetReader closing");
+            in = null; // this is synchronized (see below)
+        }
         
-        public void run() {
+        public synchronized void run() {
             ByteBuffer b = ByteBuffer.wrap(new byte[1024]);
             boolean inPkt = false;
             int aByte;
 
-            while(true) {
+            while(this.in != null) { // synchronized so we notice a close here
+                System.out.println("[debug] packetReader looking for packets");
                 try {
-                    while( (aByte = this.in.read()) > -1 ) {
+                    if( this.in.available() >= 1 ) {
+                        while( (aByte = this.in.read()) > -1 ) {
+                            System.out.printf("got aByte=%d%n", aByte);
 
-                        if( inPkt ) {
-                            try { b.put( (byte) aByte ); }
-                            catch(BufferOverflowException e) { /* huh... just ignore this for now */ }
-
-                            // TODO: do something once we have enough for a packet
-                            // probably build a packet and notify
-
-                            if( XBeePacket.enoughForPacket(b) ) {
-                                byte[] pktbytes = new byte[ b.position() ];
-                                XBeePacket p = new XBeePacket(pktbytes);
-
-                                if( p.check_checksum() )
-                                    ev.recvPacket(p);
-
-                                // else
-                                    // log this or something ... we got a packet, but apparently it's bad. :(
-
-                                inPkt = false;
-                            }
-
-                        } else {
-                            if( aByte == 0x7e ) {
-                                // hey, a new frame (hopefully)
-                                b.clear();
+                            if( inPkt ) {
                                 try { b.put( (byte) aByte ); }
-                                catch(BufferOverflowException e) { /* ignore */ }
-                                inPkt = true;
-                            }
-                        }
+                                catch(BufferOverflowException e) { /* huh... just ignore this for now */ }
 
+                                // TODO: do something once we have enough for a packet
+                                // probably build a packet and notify
+
+                                if( XBeePacket.enoughForPacket(b) ) {
+                                    byte[] pktbytes = new byte[ b.position() ];
+                                    XBeePacket p = new XBeePacket(pktbytes);
+
+                                    System.out.println("[debug] packetReader completed XBeePacket, sending to ev");
+
+                                    if( p.check_checksum() )
+                                        ev.recvPacket(p);
+
+                                    // else
+                                        // log this or something ... we got a packet, but apparently it's bad. :(
+
+                                    inPkt = false;
+                                }
+
+                            } else {
+                                if( aByte == 0x7e ) {
+                                    System.out.println("[debug] packetReader found a frame delimiter, starting packet");
+
+                                    b.clear();
+                                    try { b.put( (byte) aByte ); }
+                                    catch(BufferOverflowException e) { /* ignore */ }
+                                    inPkt = true;
+                                }
+                            }
+
+                        }
                     }
-                } 
+                }
 
                 catch(IOException e) {
                     inPkt = false;
                     // pfft.  Just start over.
                 }
+
+                try { Thread.sleep(250); }
+                catch(InterruptedException e) { /* don't really care if it doesn't work... maybe a warning should go here */ }
+
             }
         }
     }
