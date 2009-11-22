9b06b83efe827351eec0b4a3e4c907f44ee1c281
Paul Miller <paul.e.miller@wmich.edu>
2009-11-22 07:20:01 -0500

    adapt these to use their new parent

--- XBeeTxPacket.java
+++ XBeeTxPacket.java
@@ -1,17 +1,12 @@
 
-public class XBeeTxPacket extends XBeePacket {
-    public static final int TX64_PAYLOAD_LIMIT = 100;
-    public static final int TX64_DST_ADDR_LEN  = 8;
-    public static final int TX64_OPTIONS_LEN   = 1;
-    public static final int TX64_HEADER_LEN    = API_MESSAGE_TYPE_LEN + FRAME_ID_LEN + TX64_DST_ADDR_LEN + TX64_OPTIONS_LEN;
-
+public class XBeeTxPacket extends XBeeRadio64Packet {
     XBeeTxPacket(byte seqno, Address64 dst, byte []payload) throws PayloadException {
         byte _dstb[] = dst.getBytes();
 
-        if( payload.length > TX64_PAYLOAD_LIMIT )
-            throw new PayloadException("asked to packetize " + payload.length + " bytes, but payloads are restricted to " + TX64_PAYLOAD_LIMIT + " bytes");
+        if( payload.length > R64_PAYLOAD_LIMIT )
+            throw new PayloadException("asked to packetize " + payload.length + " bytes, but payloads are restricted to " + R64_PAYLOAD_LIMIT + " bytes");
 
-        int content_length = payload.length + TX64_HEADER_LEN;
+        int content_length = payload.length + R64_HEADER_LEN;
         packet = new byte[ FRAME_HEADER_LEN + content_length ];
 
         // frame header:
@@ -31,15 +26,5 @@ public class XBeeTxPacket extends XBeePacket {
             packet[i+14] = payload[i]; // 14-n
 
         packet[packet.length-1] = this.calculateChecksum();
-    }
-
-    public int payloadLength() {
-        if( !conditionalCheckPacket() )
-            return -1;
-
-        int pktlen = (packet[1] << 8) + packet[2];
-            pktlen -= TX64_HEADER_LEN;
-
-        return pktlen;
     }
 }
