df051d50789f741e298ed95d6175e332c961f2cf
Paul Miller <paul.e.miller@wmich.edu>
2009-11-22 07:18:27 -0500

    bug

---
+++ XBeeRadio64Packet.java
@@ -0,0 +1,17 @@
+
+public class XBeeRadio64Packet extends XBeePacket {
+    public static final int R64_PAYLOAD_LIMIT = 100;
+    public static final int R64_DST_ADDR_LEN  = 8;
+    public static final int R64_OPTIONS_LEN   = 1;
+    public static final int R64_HEADER_LEN    = API_MESSAGE_TYPE_LEN + FRAME_ID_LEN + R64_DST_ADDR_LEN + R64_OPTIONS_LEN;
+
+    public int payloadLength() {
+        if( !conditionalCheckPacket() )
+            return -1;
+
+        int pktlen = (packet[1] << 8) + packet[2];
+            pktlen -= R64_HEADER_LEN;
+
+        return pktlen;
+    }
+}
