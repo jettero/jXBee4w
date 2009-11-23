4ff4d3eb04203235ff3099a629c4a58c44f0f67b
Paul Miller <paul.e.miller@wmich.edu>
2009-11-22 20:46:49 -0500

    I forgot this works.  We have all the information needed to construct the NetworkEndpointInterface

--- enumerate_test.java
+++ enumerate_test.java
@@ -3,14 +3,19 @@ import gnu.io.*;
 import java.util.*;
 
 public class enumerate_test {
+    public static void configure(CommPortIdentifier pid, String portName) {
+        System.out.printf("found serial port: %s aka %s%n", pid, portName);
+    }
+
     public static void main(String[] args) {
+        enumerate_test r = new enumerate_test();
 
         Enumeration portIdentifiers = CommPortIdentifier.getPortIdentifiers();
         while (portIdentifiers.hasMoreElements()) {
             CommPortIdentifier pid = (CommPortIdentifier) portIdentifiers.nextElement();
 
             if(pid.getPortType() == CommPortIdentifier.PORT_SERIAL )
-                System.out.println("found serial port: " + pid + " aka " + pid.getName() );
+                configure(pid, pid.getName());
         }
 
     }
