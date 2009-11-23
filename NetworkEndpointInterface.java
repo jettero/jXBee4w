fd85d211682b468af3ee739a559339f5b450baa5
Paul Miller <paul.e.miller@wmich.edu>
2009-11-22 21:54:48 -0500

    this really might be how it starts

--- NetworkEndpointInterface.java
+++ NetworkEndpointInterface.java
@@ -1,2 +1,10 @@
 // This object interfaces with the XBee radios.
 
+public class NetworkEndpointInterface {
+    XBeeHandle urgent;
+    XBeeHandle mundane;
+
+    NetworkEndpointInterface() {
+        XBeeHandle.findRadio();
+    }
+}
