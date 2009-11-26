c9361c46b0fc7adb4b750be28928d2723d9a4eb6
Paul Miller <paul.e.miller@wmich.edu>
2009-11-26 15:01:24 -0500

    use the new named announce

--- neh_test.java
+++ neh_test.java
@@ -8,15 +8,8 @@ public class neh_test {
                 if( !debug.equals("0") )
                     NetworkEndpointHandle.debug = true;
 
-        NetworkEndpointHandle lhs = NetworkEndpointHandle.configuredEndpoint();
-            System.out.println("LHS Address: " + lhs.addr().toText());
-            System.out.println(" HV version: " + lhs.hardwareVersion());
-            System.out.println(" VR version: " + lhs.firmwareVersion());
-
-        NetworkEndpointHandle rhs = NetworkEndpointHandle.configuredEndpoint();
-            System.out.println("RHS Address: " + rhs.addr().toText());
-            System.out.println(" HV version: " + rhs.hardwareVersion());
-            System.out.println(" VR version: " + rhs.firmwareVersion());
+        NetworkEndpointHandle lhs = NetworkEndpointHandle.configuredEndpoint("LHS", true);
+        NetworkEndpointHandle rhs = NetworkEndpointHandle.configuredEndpoint("RHS", true);
 
         lhs.close();
         rhs.close();
