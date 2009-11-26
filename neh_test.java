96abb8483798506a417112e7e0af05c7e7456ed2
Paul Miller <paul.e.miller@wmich.edu>
2009-11-26 11:15:55 -0500

    use the configure-er

--- neh_test.java
+++ neh_test.java
@@ -2,6 +2,6 @@
 public class neh_test {
     public static void main(String args[]) {
         NetworkEndpointHandle.debug = true;
-        NetworkEndpointHandle h = new NetworkEndpointHandle();
+        NetworkEndpointHandle h = NetworkEndpointHandle.configuredEndpoint();
     }
 }
