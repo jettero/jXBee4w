dce41415bbb1877bfb6064372e7230f2ebce2849
Paul Miller <paul.e.miller@wmich.edu>
2009-11-26 13:49:23 -0500

    hrm, this still doesn't quite work reliably

--- neh_test.java
+++ neh_test.java
@@ -1,7 +1,13 @@
 
 public class neh_test {
     public static void main(String args[]) {
-        NetworkEndpointHandle.debug = true;
+        String debug = System.getenv("DEBUG");
+
+        if( debug != null )
+            if( !debug.isEmpty() )
+                if( !debug.equals("0") )
+                    NetworkEndpointHandle.debug = true;
+
         NetworkEndpointHandle h = NetworkEndpointHandle.configuredEndpoint();
 
         System.out.println("Cool! All configured! Address: " + h.addr().toText());
