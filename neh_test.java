9c3994e5a107f2e2a7695be9ba0f5540a0171836
Paul Miller <paul.e.miller@wmich.edu>
2009-11-26 11:43:41 -0500

    be true for now

--- neh_test.java
+++ neh_test.java
@@ -1,7 +1,7 @@
 
 public class neh_test {
     public static void main(String args[]) {
-        NetworkEndpointHandle.debug = false;
+        NetworkEndpointHandle.debug = true;
         NetworkEndpointHandle h = NetworkEndpointHandle.configuredEndpoint();
 
         System.out.println("Cool! All configured! Address: " + h.addr().toText());
