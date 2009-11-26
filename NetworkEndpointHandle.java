7b1929c2819c17477de6dd8301e0b7b3b66a0e14
Paul Miller <paul.e.miller@wmich.edu>
2009-11-26 10:19:46 -0500

    this might compile

--- NetworkEndpointHandle.java
+++ NetworkEndpointHandle.java
@@ -1,28 +1,41 @@
 import gnu.io.*;
 import java.util.*;
 
-public class NetworkEndpointHandle extends XBeeHandle {
-    private static List<CommPortIdentifier> ports;
+public class NetworkEndpointHandle {
+    private static Queue<CommPortIdentifier> ports;
+    private XBeeHandle xh;
 
     private static void populatePortNames() {
         if( ports != null )
             return;
 
-        ports = new ArrayList<CommPortIdentifier>();
+        ports = new ArrayDeque<CommPortIdentifier>();
 
         Enumeration portIdentifiers = CommPortIdentifier.getPortIdentifiers();
         while (portIdentifiers.hasMoreElements()) {
             CommPortIdentifier pid = (CommPortIdentifier) portIdentifiers.nextElement();
 
             if(pid.getPortType() == CommPortIdentifier.PORT_SERIAL )
-                p.add(pid);
+
+                ports.offer(pid); // returns false if it's full, but who cares, we're expecting like 8 things tops
         }
     }
 
-    private static void locateAndConfigure() {
+    private static CommPortIdentifier locateAndConfigure() {
+        CommPortIdentifier pid;
+
         populatePortNames();
 
-        while( !ports.isEmpty() ) {
+        while( (pid = ports.poll()) != null )
+            System.out.println("kk: " + pid.getName());
+
+        return null;
+    }
+
+    NetworkEndpointHandle() {
+        CommPortIdentifier meh = locateAndConfigure();
+        // XBeeConfig = new XBeeConfig();
+        // xh = new XBeeHandle();
     }
 
 }
