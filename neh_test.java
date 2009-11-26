160eb61d4aa7415b0ffe59e0214d86613bc3ee49
Paul Miller <paul.e.miller@wmich.edu>
2009-11-26 10:44:28 -0500

    debug

--- neh_test.java
+++ neh_test.java
@@ -1,6 +1,7 @@
 
 public class neh_test {
     public static void main(String args[]) {
+        NetworkEndpointHandle.debug = true;
         NetworkEndpointHandle h = new NetworkEndpointHandle();
     }
 }
