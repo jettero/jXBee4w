e092edcc69f7e58941f816afa8d301c7b72e34b5
Paul Miller <paul.e.miller@wmich.edu>
2009-11-26 10:25:36 -0500

    fixed this up for Queue (vs List)

--- packetizer_test.java
+++ packetizer_test.java
@@ -4,15 +4,17 @@ import java.util.*;
 public class packetizer_test {
     public static void main(String[] args) {
         String payload = "";
-        XBeePacketizer p = new XBeePacketizer();
+        XBeePacketizer packetizer = new XBeePacketizer();
         Address64 dst = new Address64("99:11:ff:cc:11:05:19:73");
+        XBeePacket packet;
 
         for(int i=0; i<557; i++)
             payload += ( (i%2)==0 ? "x" : "o");
 
-        List q = p.tx(dst, payload);
+        Queue q = packetizer.tx(dst, payload);
 
-        for(int i=0; i<q.size(); i++)
-            ( (XBeePacket) q.get(i) ).fileDump("packet-%d.pkt");
+
+        while( (packet = (XBeePacket) q.poll()) != null )
+            packet.fileDump("packet-%d.pkt");
     }
 }
