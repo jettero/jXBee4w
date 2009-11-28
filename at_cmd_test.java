4f0126989472e5a14d34abf2bc32dcd1afd6348a
Paul Miller <paul.e.miller@wmich.edu>
2009-11-28 08:36:45 -0500

    make some example packets for my config rewrite

--- at_cmd_test.java
+++ at_cmd_test.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class at_cmd_test {
     public static void main(String[] args) {
         String s[][] = {
-            {"DB"}, {"BD", "7"}, {"AP", "1"}
+            {"DB"}, {"BD", "7"}, {"AP", "1"}, {"AP"}, {"BD"}, {"VR"}
         };
         XBeePacketizer z = new XBeePacketizer();
         XBeePacket p[] = z.at(s);
