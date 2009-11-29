5d9d927bdcb3ce5798efec5be16bc44edf720879
Paul Miller <paul.e.miller@wmich.edu>
2009-11-29 16:24:23 -0500

    k, need to use this in the handles

---
+++ XBeeTxStatusPacket.java
@@ -0,0 +1,18 @@
+
+public class XBeeTxStatusPacket extends XBeePacket {
+    public static final int TXS_HEADER_LEN    = API_MESSAGE_TYPE_LEN + FRAME_ID;
+    public static final int TXS_FRAME_ID_BYTE = FRAME_HEADER_LEN + API_MESSAGE_TYPE_LEN;
+    public static final int TXS_STATUS_BYTE   = TX_FRAMEID_BYTE + FRAME_ID_LEN;
+
+    public static final byte TXS_STATUS_SUCCESS   = 0x0;
+    public static final byte TXS_STATUS_NO_ACK    = 0x1;
+    public static final byte TXS_STATUS_CCA_ERROR = 0x2; // cyclic sleep ode cyclic-sleep
+    public static final byte TXS_STATUS_PURGED    = 0x3; // coordinator timeout
+
+    XBeeTxStatusPacket(byte b[]) { super(b); }
+
+    public boolean statusOK()     { return packet[TXS_STATUS_BYTE] == TXS_STATUS_SUCCESS; }
+    public boolean statusNOACK()  { return packet[TXS_STATUS_BYTE] == TXS_STATUS_NO_ACK;  }
+    public boolean statusCCAERR() { return packet[TXS_STATUS_BYTE] == TXS_STATUS_CCAERR;  }
+    public boolean statusPURGED() { return packet[TXS_STATUS_BYTE] == TXS_STATUS_PURGED;  }
+}
