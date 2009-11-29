5fdcb1db4926f5feee3ca215a29d462d17698e38
Paul Miller <paul.e.miller@wmich.edu>
2009-11-29 08:21:10 -0500

    return some empty stuff until the rest of the infrastructure is ready

--- XBeeRxPacket.java
+++ XBeeRxPacket.java
@@ -1,4 +1,14 @@
 
 public class XBeeRxPacket extends XBeeRadio64Packet {
     XBeeRxPacket(byte b[]) { super(b); }
+
+    public Address64 getSourceAddress() {
+        Address64 a = new Address64(new byte[0], new byte[0]); // should just fill with 0s
+
+        return a;
+    }
+
+    public byte[] getPayloadBytes() {
+        return new byte[0];
+    }
 }
