a4df4608a78c635cb5cabd3babe437b33f26cede
Paul Miller <paul.e.miller@wmich.edu>
2009-11-29 17:07:14 -0500

    various spelling errors and bugs

--- XBeeTxStatusPacket.java
+++ XBeeTxStatusPacket.java
@@ -1,8 +1,8 @@
 
 public class XBeeTxStatusPacket extends XBeePacket {
-    public static final int TXS_HEADER_LEN    = API_MESSAGE_TYPE_LEN + FRAME_ID;
+    public static final int TXS_HEADER_LEN    = API_MESSAGE_TYPE_LEN + FRAME_ID_LEN;
     public static final int TXS_FRAME_ID_BYTE = FRAME_HEADER_LEN + API_MESSAGE_TYPE_LEN;
-    public static final int TXS_STATUS_BYTE   = TX_FRAMEID_BYTE + FRAME_ID_LEN;
+    public static final int TXS_STATUS_BYTE   = TXS_FRAME_ID_BYTE + FRAME_ID_LEN;
 
     public static final byte TXS_STATUS_SUCCESS   = 0x0;
     public static final byte TXS_STATUS_NO_ACK    = 0x1;
@@ -11,8 +11,8 @@ public class XBeeTxStatusPacket extends XBeePacket {
 
     XBeeTxStatusPacket(byte b[]) { super(b); }
 
-    public boolean statusOK()     { return packet[TXS_STATUS_BYTE] == TXS_STATUS_SUCCESS; }
-    public boolean statusNOACK()  { return packet[TXS_STATUS_BYTE] == TXS_STATUS_NO_ACK;  }
-    public boolean statusCCAERR() { return packet[TXS_STATUS_BYTE] == TXS_STATUS_CCAERR;  }
-    public boolean statusPURGED() { return packet[TXS_STATUS_BYTE] == TXS_STATUS_PURGED;  }
+    public boolean statusOK()       { return packet[TXS_STATUS_BYTE] == TXS_STATUS_SUCCESS;   }
+    public boolean statusNoACK()    { return packet[TXS_STATUS_BYTE] == TXS_STATUS_NO_ACK;    }
+    public boolean statusCCAError() { return packet[TXS_STATUS_BYTE] == TXS_STATUS_CCA_ERROR; }
+    public boolean statusPURGED()   { return packet[TXS_STATUS_BYTE] == TXS_STATUS_PURGED;    }
 }
