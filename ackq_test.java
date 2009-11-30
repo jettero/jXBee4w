e2b0bf5cb9866fb79e97c75e5691943980f5e610
Paul Miller <paul.e.miller@wmich.edu>
2009-11-30 11:52:39 -0500

    next test

--- ackq_test.java
+++ ackq_test.java
@@ -1,6 +1,17 @@
+import java.util.*;
 
 public class ackq_test {
     public static void main(String arg[]) {
-        System.out.println("Hello world");
+        Address64 a = new Address64();
+        XBeePacketizer p = new XBeePacketizer();
+        String longString = "longString0 ";
+
+        for(int i=1; i<=100; i++)
+            longString += "longString" + i + " ";
+
+        Queue q = p.tx( a, longString );
+        ACKQueue Q = new ACKQueue(q);
+
+        System.out.println(Q.size());
     }
 }
