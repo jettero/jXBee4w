cfdb036bbc4ffb430cd30b3ee9e0619d84357c54
Paul Miller <paul.e.miller@wmich.edu>
2009-11-30 17:13:19 -0500

    my offset byte might be flawed...

--- fragment_test.java
+++ fragment_test.java
@@ -4,5 +4,8 @@ public class fragment_test {
         String longString = "Hello mang, this is my string.";
         System.out.printf("--fragmenting %d bytes:%n", longString.length());
         byte b[][] = Message.fragmentMessage(longString, 7);
+
+        for(int i=0; i<b.length; i++)
+            XBeePacket.bytesToFile( String.format("fragment-%02x.dat", b[i][b[i].length-1]), b[i] );
     }
 }
