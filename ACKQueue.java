c42f1e636485a261202f9367b0e01afa87bad4fa
Paul Miller <paul.e.miller@wmich.edu>
2009-11-30 11:53:03 -0500

    I wanted a treemap, not a hashmap.  the treemaps are sorted

--- ACKQueue.java
+++ ACKQueue.java
@@ -1,22 +1,25 @@
+import java.util.*;
 
 public class ACKQueue {
-    private HashMap <Integer, XBeePacket> Q = new TreeMap<Integer, XBeePacket>();
+    private TreeMap <Integer, XBeePacket> Q = new TreeMap<Integer, XBeePacket>();
 
     ACKQueue(Queue packets) {
         XBeePacket p;
-        while( (p = packets.poll) != null )
+
+        while( (p = (XBeePacket) packets.poll()) != null )
+
             Q.put( new Integer(p.frameID()), p );
     }
 
     public int size() {
-        return q.size();
+        return Q.size();
     }
 
     public XBeePacket[] packets() {
         return (XBeePacket[]) Q.values().toArray();
     }
 
-    public void ACK(int frameID) {
+    public synchronized void ACK(int frameID) {
         Q.remove(new Integer(frameID));
     }
 }
