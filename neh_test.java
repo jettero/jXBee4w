eadbac74ba0c84822355675b63b38ac378ea6297
Paul Miller <paul.e.miller@wmich.edu>
2009-12-01 13:02:01 -0500

    we don't use this form anymore

--- neh_test.java
+++ neh_test.java
@@ -1,13 +1,6 @@
 
 public class neh_test {
     public static void main(String args[]) {
-        String debug = System.getenv("DEBUG");
-
-        if( debug != null )
-            if( !debug.isEmpty() )
-                if( !debug.equals("0") )
-                    NetworkEndpointHandle.debug = true;
-
         NetworkEndpointHandle lhs = NetworkEndpointHandle.configuredEndpoint("LHS", true);
         NetworkEndpointHandle rhs = NetworkEndpointHandle.configuredEndpoint("RHS", true);
 
