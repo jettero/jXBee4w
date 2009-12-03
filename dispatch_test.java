ad3ba4bd35a2ca2d61384a1149105e5e72343234
Paul Miller <paul.e.miller@wmich.edu>
2009-12-02 21:06:43 -0500

    I tried setting the channel in here... seems to work, who knows

--- dispatch_test.java
+++ dispatch_test.java
@@ -4,6 +4,14 @@ public class dispatch_test {
         XBeeDispatcher lhs = XBeeDispatcher.configuredDispatcher("LHS", true);
         XBeeDispatcher rhs = XBeeDispatcher.configuredDispatcher("RHS", true);
 
+        try {
+
+        lhs.setChannel( 0x0e );
+        rhs.setChannel( 0x0e );
+        Thread.sleep(2000);
+
+        } catch(Exception e) { System.err.println("pfft... " + e.getMessage()); }
+
         lhs.close();
         rhs.close();
     }
