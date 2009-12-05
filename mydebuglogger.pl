5e75ddc720ef29f29973887cba1c11ce31d96361
Paul Miller <paul.e.miller@wmich.edu>
2009-12-05 16:54:31 -0500

    cleanups

--- mydebuglogger.pl
+++ mydebuglogger.pl
@@ -1,16 +1,20 @@
 #!/usr/bin/perl
 
 use strict;
+use POSIX;
 
 $| = 1;
 
 open my $out, ">", "run.log" or die $!;
 
 while(<>) {
-    my $time = localtime;
+    my $time = strftime('%M:%S', localtime);
+
+    s/00:13:a2:00:40/xbee/g; # all these MACs are the same
 
     s/[\r\n]$//g;
+    my $debug = s/\[debug\] //;
 
     print $out "$time: $_\n";
-    print "$_\n" unless m/\[debug\]/;
+    print "$_\n" unless $debug;
 }
