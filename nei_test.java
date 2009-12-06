8c61c9e7bb284f86c08ba8725df0763791e0efd4
Paul Miller <paul.e.miller@wmich.edu>
2009-12-06 10:39:42 -0500

    error out without a name, use name to build test

--- nei_test.java
+++ nei_test.java
@@ -1,8 +1,13 @@
 
 public class nei_test {
     public static void main(String [] args) {
-        NetworkEndpointInterface NEI = new NetworkEndpointInterface("L1", "ranger", 4000);
+        if( args.length != 1 ) {
+            System.out.println("java nei_test <name>");
+            System.exit(1);
+        }
 
-        System.out.printf("NEI running%n");
+        NetworkEndpointInterface NEI = new NetworkEndpointInterface(args[0], "ranger", 4000);
+
+        System.out.printf(args[0] + " NEI running%n");
     }
 }
