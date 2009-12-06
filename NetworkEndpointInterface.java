85dd1db6a74b1ad92758b2f7dd88c0d7e8b5e5fc
Paul Miller <paul.e.miller@wmich.edu>
2009-12-06 08:49:42 -0500

    I think this actually works

--- NetworkEndpointInterface.java
+++ NetworkEndpointInterface.java
@@ -1,44 +1,24 @@
 // This object interfaces with the XBee radios.
 
-
 public class NetworkEndpointInterface {
     private XBeeDispatcher urgent;
     private XBeeDispatcher mundane;
+    String name;
+    NCIClient NCI;
 
     private static class NCIClient extends LineOrientedClient {
         NetworkEndpointInterface NEI;
-        Socket server;
-        PrintWriter out;
 
-        NCIClient(String _sh, int _p, NetworkEndpointInterface _n) throws UnknownHostException, IOException {
-            NEI = _n;
-
-            try {
-                server            = new Socket(_sh, _p);
-                out               = new PrintWriter(server.getOutputStream(), true);
-                BufferedReader in = new BufferedReader(new InputStreamReader(server.getInputStream()));
-
-            } catch (UnknownHostException e) {
-                System.err.println("ERROR connecting to NCI: " + e.getMessage());
-                System.exit(1);
-
-            } catch (IOException e) {
-                System.err.println("ERROR building streams from connection to NCI: " + e.getMessage());
-                System.exit(1);
-            }   
-
+        NCIClient(String _sh, int _p, NetworkEndpointInterface _e) {
+            super(_sh, _p);
+            NEI = _e;
         }
     }
 
-
-
-    NetworkEndpointInterface(String name) {
-        NCIClient = new NCIClient( "ranger", 4000, this );
-        try {
-            urgent  = new XBeeDispatcher();
-            mundane = new XBeeDispatcher();
-
-        } catch(XBeeNotFoundException e) {
-        }
+    NetworkEndpointInterface(String _n, String host, int port) {
+        name    = _n;
+        NCI     = new NCIClient( host, port, this );
+        urgent  = XBeeDispatcher.configuredDispatcher(name + ":urgent",  true);
+        mundane = XBeeDispatcher.configuredDispatcher(name + ":mundane", true);
     }
 }
--- nei_test.java
+++ nei_test.java
@@ -1,7 +1,7 @@
 
 public class nei_test {
     public static void main(String [] args) {
-        NetworkEndpointInterface NEI = new NetworkEndpointInterface("ranger", 4000);
+        NetworkEndpointInterface NEI = new NetworkEndpointInterface("L1", "ranger", 4000);
 
         System.out.printf("NEI running%n");
     }
