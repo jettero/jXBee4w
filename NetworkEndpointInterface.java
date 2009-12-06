ec33877e5b379bf866f12ad2b9fc06143b379c9e
Paul Miller <paul.e.miller@wmich.edu>
2009-12-05 22:08:56 -0500

    goign to bed

--- NetworkEndpointInterface.java
+++ NetworkEndpointInterface.java
@@ -5,6 +5,13 @@ public class NetworkEndpointInterface {
     private XBeeDispatcher urgent;
     private XBeeDispatcher mundane;
 
+    private Hashtable <String, Address64> knownNeighbors   = new Hashtable <String, Address64>();
+    private Hashtable <Address64, String> reverseNeighbors = new Hashtable <Address64, String>();
+    private HashSet   <Address64> unknownNeighbors = new HashSet <Address64> ();
+
+    private static class NCIHandler {
+    }
+
     NetworkEndpointInterface(String name) throws XBeeNotFoundException {
         urgent  = new XBeeDispatcher();
         mundane = new XBeeDispatcher();
