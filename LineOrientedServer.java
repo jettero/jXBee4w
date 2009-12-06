342e943a64b283e3bdb310dccc0e309942f1f8d2
Paul Miller <paul.e.miller@wmich.edu>
2009-12-05 19:36:11 -0500

    this might be close enough to comile, who knows, dinner time

--- LineOrientedServer.java
+++ LineOrientedServer.java
@@ -11,12 +11,48 @@ public class LineOrientedServer {
             code = _c;
             msg = _m;
         }
+
+        public String toLine() {
+            return String.format("%03d %s", code, msg);
+        }
     }
 
-    public static class LineDispatcher {
+    public static interface HandleLineEvent {
+        public CommandResponse handleCommand(String cmd);
     }
+
+    public static class LineDispatcher implements Runnable {
+        private Socket client;
+        private PrintWriter out;
+        private BufferedReader in;
+        private HandleLineEvent cmdHandler;
+
+        LineDispatcher(Socket s, HandleLineEvent _e) {
+            out = new PrintWriter(s.getOutputStream(), true);
+             in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+
+            client = s;
+            cmdHandler = _e;
+        }
+
+        public void run() {
+            String line;
+
+            while( (line = in.readLine()) != null )
+                out.println( cmdHandler.handleCommand(line).toLine() );
+
+            out.close();
+            in.close();
+            client.close();
+        }
+    }
+
 
     ServerSocket serverSocket;
+
+    public CommandResponse handleCommand(String ignored) {
+        return new CommandResponse(UNIMPLEMENTED_ERROR, "command handler unimplemented");
+    }
 
     LineOrientedServer(int listenPort) {
         try {
@@ -24,7 +60,7 @@ public class LineOrientedServer {
             Socket clientSocket;
 
             while( (clientSocket = serverSocket.accept()) != null ) {
-                System.out.println("do stuff here");
+                (new Thread( new LineDispatcher(clientSocket, this) )).start();
             }
 
         } catch (IOException e) {
